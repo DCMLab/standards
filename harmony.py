c238a78fcb5c9af4e9fc59a7a8a617118c3a7983
laserjeyes <johannes.hentschel@epfl.ch>
2020-04-08 18:35:42 +0200

    removed 9th chords and trailing Julia-x

--- harmony.py
+++ harmony.py
@@ -5,9 +5,9 @@ regex = r"""^
     (?P<chord>
     (?P<numeral>(b*|\#*)(VII|VI|V|IV|III|II|I|vii|vi|v|iv|iii|ii|i|Ger|It|Fr))
     (?P<form>[%o+M])?
-    (?P<figbass>(9|7|65|43|42|2|64|6))?
+    (?P<figbass>(7|65|43|42|2|64|6))?
     (\((?P<changes>(\+?(b*|\#*)\d)+)\))?
     (/\.?(?P<relativeroot>(b*|\#*)(VII|VI|V|IV|III|II|I|vii|vi|v|iv|iii|ii|i)))?)
     (?P<pedalend>\])?
     (?P<phraseend>\\\\)?$
-    """x
+    """
