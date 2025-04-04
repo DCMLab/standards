6fa0b60a9825dd772ad7bfc7d212b3bd734a6493
Johannes Hentschel <johannes.hentschel@epfl.ch>
2025-04-04 23:36:18 +0200

    updates the Makefile with the contents of https://github.com/davidstutz/sphinx-example/blob/master/docs/Makefile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -148,3 +148,5 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+latex_engine = 'xelatex'
